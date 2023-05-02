25b8d73381ddbd3069a62743d68ff14e8de4818f
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-02 09:50:13 -0500

    Delete Seccion_004 directory

--- Seccion_004/Ejercicio_03_Mayor_Edad.py
+++
@@ -1,7 +0,0 @@
-edadAdulto = 18
-edadPersona = int(input("Proporciona tu edad: "))
-
-if edadPersona >= edadAdulto:
-    print(f'La persona con edad {edadPersona} es un adulto')
-else:
-    print(f'La persona con edad {edadPersona} es menor de edad')
