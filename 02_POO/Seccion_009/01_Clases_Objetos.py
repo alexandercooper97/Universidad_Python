f287057479d8a3e932620c26912b15a7a23a5792
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-05 09:17:09 -0500

    Clases y Objetos

---
+++ 02_POO/Seccion_009/01_Clases_Objetos.py
@@ -0,0 +1,15 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def desplegar(self):
+        #print(f'Nombre: {self.nombre}, Edad: {self.edad}')
+        print("Nombre :", self.nombre)
+        print("Edad : ",self.edad)
+
+
+persona1 = Persona('Juan', 28)
+persona1.desplegar()
+persona2 = Persona('Karla', 30)
+persona2.desplegar()
