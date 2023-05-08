f2e893253f69e796f730001dfa9ceb8acc422132
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-07 22:42:59 -0500

    Clases y Objetos

---
+++ 02_POO/Seccion_009/Ejercicio_03_Cubo.py
@@ -0,0 +1,17 @@
+class Cubo:
+	def __init__(self, ancho, alto, profundo):
+		self.ancho = ancho
+		self.alto = alto
+		self.profundo = profundo
+		
+	def calcular_volumen(self):
+		return self.ancho * self.alto * self.profundo
+
+
+if __name__=="__main":
+	ancho = int(input('Proporciona el ancho: '))
+	alto = int(input('Proporciona el alto: '))
+	profundo = int(input('Proprociona lo profundo: '))
+
+	cubo1 = Cubo(ancho, alto, profundo)
+	print(f'Volúmen cubo: {cubo1.calcular_volumen()}')
